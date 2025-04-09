fbdbd1db852f15788f665b0aa82097787fe0dfde
Rimuru <velocityontopfrfr@gmail.com>
2025-04-09 19:09:32 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,11 @@
-local scripts={
-    [73956553001240]="Volleyball_Legends",
-    [18687417158]="Forsaken"
+local scripts = {
+    [73956553001240] = "Volleyball_Legends", -- Add comma here
+    [18687417158] = "Forsaken" -- Fixed typo (no extra 'd')
 }
 
-local game_script=scripts[game.GameId]
+local game_script = scripts[game.GameId]
 if game_script then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RimuruOnDiscord/Halcyon/refs/heads/main/"..game_script..".lua"))()
 else
-    game:GetService"Players".LocalPlayer:Kick("This game is not supported by Halcyon.")
+    game:GetService("Players").LocalPlayer:Kick("This game is not supported by Halcyon.")
 end
