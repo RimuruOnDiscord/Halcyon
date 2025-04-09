51295061c4ca400ca5a94e6a987afa4c575fa865
Rimuru <velocityontopfrfr@gmail.com>
2025-04-09 15:52:39 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local scripts={
+    [73956553001240]="Anime_Spirits_Journey"
+}
+
+local game_script=scripts[game.GameId]
+if game_script then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RimuruOnDiscord/Halcyon/refs/heads/main/"..game_script..".lua"))()
+else
+    game:GetService"Players".LocalPlayer:Kick("This game is not supported by Halcyon.")
+end
